4eb0d84432f293c2c3f699cd0035e98c8fe89af6
BPRO <iserver12345@gmail.com>
2020-08-13 20:45:00 +0500

    Add state class

--- app/handlers/create_post.py
+++ app/handlers/create_post.py
@@ -1,28 +1,34 @@
 from aiogram.types import CallbackQuery, Message
 from aiogram.dispatcher import FSMContext
 import datetime
-
-from config import bot, dp, colors, text, image_path, adminID
+from aiogram import types
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from config import bot, dp, colors, adminID
 from keyboard.post import choose_screen_color_kb
 from functions.post import create_post
 
 
-@dp.message_handler(commands='cp')
+class Post(StatesGroup):
+    text = State()
+    image_path = State()
+
+
+@dp.message_handler(commands="cp", state="*")
 async def cmd_start(message: Message):
     if message.from_user.id == adminID:
-        await text.set()
+        await Post.text.set()
         await message.reply("Введи текст нового посту")
 
 
-@dp.message_handler(state=text)
+@dp.message_handler(state=Post.text)
 async def get_text(message: Message, state: FSMContext):
     await state.update_data(text=message.text)
     await message.answer("color:", reply_markup=choose_screen_color_kb())
+    await Post.image_path.set()
 
 
-@dp.callback_query_handler(lambda c: c.data in colors, state=image_path)
+@dp.callback_query_handler(lambda c: c.data in colors, state=Post.image_path)
 async def _(c: CallbackQuery, state: FSMContext):
-    await image_path.set()
     today = datetime.datetime.now().strftime("%A")
     path = f'images/{c.data}/{today.lower()}.jpg'
     await state.update_data(image_path=path)
