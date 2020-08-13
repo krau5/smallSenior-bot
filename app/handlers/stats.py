93ac0d9665870f0d7c4515a2e59e7a29fb41da22
BPRO <iserver12345@gmail.com>
2020-08-13 20:44:16 +0500

    Update stats.py

--- app/handlers/stats.py
+++ app/handlers/stats.py
@@ -1,5 +1,4 @@
 from aiogram.types import Message
-# from aiogram.dispatcher import FSMContext
 
 from functions.stats import count_members
 from config import dp
@@ -7,4 +6,4 @@ from config import dp
 
 @dp.message_handler(commands=['stats'], state="*")
 async def stats(message: Message):
-    message.answer(count_members())
+    await message.answer(await count_members())
